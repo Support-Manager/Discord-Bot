df6814f52fde591f9f0682f428013961a6017d3a
Linus Bartsch <frenki57a@gmail.com>
2019-02-04 21:19:36 +0100

    fixed dbl api stats posting to work with sharding

--- bot/bot.py
+++ bot/bot.py
@@ -5,7 +5,7 @@ import os
 from .context import Context
 from .properties import CONFIG
 import asyncio
-import dbl
+import aiohttp
 
 
 logger = logging.getLogger(__name__)
@@ -15,7 +15,7 @@ class Bot(commands.AutoShardedBot):
     def __init__(self, *args, **kwargs):
         super(Bot, self).__init__(*args, **kwargs)
 
-        self.dbl = dbl.Client(bot=self, token=CONFIG['dbl_api_token'])
+        self.dbl_session = aiohttp.ClientSession(loop=self.loop, headers={'Authorization': CONFIG['dbl_api_token']})
         self.loop.create_task(self._update_stats())
 
     with open(os.path.dirname(__file__) + '/translations/strings.yml', 'r', encoding='utf-8') as stream:
@@ -29,14 +29,28 @@ class Bot(commands.AutoShardedBot):
 
         await self.wait_until_ready()
 
+        dbl_api_url = f'https://discordbots.org/api/bots/{self.user.id}/stats'
+        stats = {'shard_count': self.shard_count}
+
         while not self.is_closed():
             logger.info('attempting to post server count')
-            try:
-                await self.dbl.post_server_count(shard_count=self.shard_count)
-                logger.info('posted server count ({})'.format(len(self.guilds)))
-            except Exception as e:
-                logger.exception('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
+            server_count = len(self.guilds)
+
+            for shard_id in self.shard_ids:
+                shard_server_count = server_count // len(self.shard_ids)
+
+                if shard_id == self.shard_ids[-1]:
+                    shard_server_count += server_count % len(self.shard_ids)
+
+                stats['shard_id'] = shard_id
+                stats['server_count'] = shard_server_count
+
+                async with self.dbl_session.post(dbl_api_url, json=stats) as resp:
+                    logger.info(f'posted server count for shard {shard_id} ({shard_server_count}; code: {resp.status})')
+
             await asyncio.sleep(1800)
+
+        await self.dbl_session.close()
 
     async def on_message(self, message):
         ctx = await self.get_context(message, cls=Context)
